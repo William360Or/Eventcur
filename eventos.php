a6fb5742faf3c75123d1ab64f9bcb9b4f30b4526
W_Fernando_OR <Williamolaya93@gmail.com>
2024-11-02 20:45:45 -0500

    Estructura inicial de archivos y pagina home programada

---
+++ eventos.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EventCur</title>
+    <link rel="stylesheet" href="style/estilos_eventos.css">
+</head>
+<body>
+<nav class="menu">
+  <ul>
+    <li><a href="index.php">Home</a></li>
+    <li class="dropdown">
+      <a href="eventos.php">Eventos</a>
+      <div class="mega-menu">
+        <div class="column">
+          <h3>Familiares</h3>
+          <div class="opciones">
+          <a href="eventos.php">Matrimonios</a>
+          <a href="eventos.php">Cumpleaños</a>
+          <a href="eventos.php">15 años</a>
+          <a href="eventos.php">Grados</a>
+          </div>
+        </div>
+        <div class="column">
+          <h3>Corporativos</h3>
+          <div class="opciones">
+          <a href="eventos.php">Conferencias</a>
+          <a href="eventos.php">Reuniones Corporativas</a>
+          <a href="eventos.php">Fiestas de fin de año</a>
+          </div>
+        </div>
+  
+      </div>
+    </li>
+    <li><a href="mievento.php">Mi Evento</a></li>
+  </ul>
+</nav>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,105 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>EventCur</title>
+    <link rel="stylesheet" href="style/estilos_index.css">
+</head>
+<body>
+    <!-- Baner !--> 
+    <section class="banner">
+    <nav class="menu">
+  <ul>
+    <li><a href="index.php">Home</a></li>
+    <li class="dropdown">
+      <a href="#">Eventos</a>
+      <div class="mega-menu">
+        <div class="column">
+          <h3>Familiares</h3>
+          <div class="opciones">
+          <a href="eventos.php">Matrimonios</a>
+          <a href="eventos.php">Cumpleaños</a>
+          <a href="eventos.php">15 años</a>
+          <a href="eventos.php">Grados</a>
+          </div>
+        </div>
+        <div class="column">
+          <h3>Corporativos</h3>
+          <div class="opciones">
+          <a href="eventos.php">Conferencias</a>
+          <a href="eventos.php">Reuniones Corporativas</a>
+          <a href="eventos.php">Fiestas de fin de año</a>
+          </div>
+        </div>
+  
+      </div>
+    </li>
+    <li><a href="mievento.php">Mi Evento</a></li>
+  </ul>
+</nav>
+        <p class="banner__eslogan">Generando Momentos</p>
+        <a href=personaliza.php class="banner__btn--personaliza">Personaliza y cotiza tu evento</a>
+    </section>
+
+     <!-- Seccion de about !-->  
+
+    <section class="about secciones">
+        <div class="about__texto">
+            <h3 class="about__titulo">Sobre Nosotros</h3>
+
+            <p>En Event Cur, nos apasiona crear experiencias inolvidables. Somos una empresa especializada en la organización y gestión de eventos, comprometidos con convertir cada idea en una realidad memorable. Desde eventos corporativos y conferencias hasta bodas y celebraciones personales, nuestro equipo se dedica a ofrecer soluciones personalizadas que superen las expectativas.</p><br>
+
+            <p>Con mas de 15 años de experiencia en el sector, contamos con un equipo de profesionales altamente capacitados en planificación, diseño y ejecución de eventos, lo que nos permite atender cada detalle con precisión. Nos enorgullece nuestra capacidad para adaptarnos a las necesidades específicas de nuestros clientes, proporcionando un servicio impecable, innovador y a medida.</p>
+        </div>
+       
+            <img src="img/about.jpg" alt="Imagen about" class="about__imagen">            
+    </section>
+
+    <!-- Seccion de mision !-->  
+
+        <section class="mision secciones">        
+            <img src="img/mision.jpg" alt="Imagen mision" class="mision__imagen">            
+        </div>
+        <div class="mision__texto">
+            <h3 class="mision__titulo">Misión</h3>
+            <p>En Event Cur, nuestra misión es transformar ideas en eventos extraordinarios. Nos comprometemos a brindar experiencias únicas y personalizadas, combinando creatividad, innovación y un enfoque detallista para que cada celebración sea un éxito memorable. Nos esforzamos por ofrecer un servicio excepcional que supere las expectativas de nuestros clientes y genere momentos inolvidables.</p>
+        </div>        
+    </section>
+
+    <!-- Seccion de Vision !--> 
+
+    <section class="vision secciones">
+        <div class="vision__texto">
+            <h3 class="vision__titulo">Visión</h3>
+
+            <p>Ser la empresa líder en la organización de eventos a nivel nacional, reconocida por nuestra capacidad de innovación, excelencia operativa y compromiso con la satisfacción del cliente. Aspiramos a ser una referencia en el sector, creando eventos que inspiren, emocionen y marquen la diferencia en la vida de las personas y organizaciones con las que trabajamos.</p>
+        </div>
+       
+            <img src="img/vision.jpg" alt="Imagen vision" class="vision__imagen">            
+    </section>
+
+    <!-- Seccion de Valores !--> 
+
+    <section class="valores secciones">
+        <div class="valores__valor">
+            <img src="img/crea.png" alt="imagen creatividad" class="valores__valor__img-creatividad">
+            <h3 class="valores__valor__titulo">CREATIVIDAD</h3>
+            <P class="valores__valor__parrafo">Nos apasiona diseñar experiencias innovadoras y originales, adaptadas a los deseos y necesidades de nuestros clientes.</P>
+        </div>
+        <div class="valores__valor">
+            <img src="img/cali.png" alt="imagen calidad" class="valores__valor__img-calidad">
+            <h3  class="valores__valor__titulo">CALIDAD</h3>
+            <P class="valores__valor__parrafo">Nos comprometemos con la excelencia en cada detalle, desde la planificación hasta la ejecución, garantizando que cada evento sea perfecto.</P>
+        </div>
+        <div class="valores__valor">
+            <img src="img/compro.png" alt="imagen compromiso" class="valores__valor__img-compromiso">
+            <h3  class="valores__valor__titulo">COMPROMISO</h3>
+            <P class="valores__valor__parrafo">La satisfacción de nuestros clientes es nuestra prioridad. Nos dedicamos a superar sus expectativas en cada proyecto.</P>
+        </div>
+    </section>
+    
+    
+<script src="scripts/script.js"></script>
+</body>
+</html>
